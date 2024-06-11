0abc30adbe0cce0729a886f644a5580a6a071a7c
Jumogehn H <jumogehn@gmail.com>
2024-06-11 19:46:01 +0900

    added upto 04/10

---
+++ 03/07/07.cpp
@@ -0,0 +1,70 @@
+﻿#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+typedef struct {
+    float x, y;
+    float vx, vy;
+    int lifetime;
+} particle_t;
+
+void spawn_particle(particle_t* particle) {
+    particle->x = rand() % SCREEN_WIDTH;
+    particle->y = rand() % SCREEN_HEIGHT;
+    particle->vx = ((float)rand() / RAND_MAX) * 2 - 1;
+    particle->vy = ((float)rand() / RAND_MAX) * 2 - 1;
+    particle->lifetime = 60 + rand() % 60;
+}
+
+int main(int argc, char* args[]) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Event event;
+    particle_t particles[100];
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Particle System",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    for (int i = 0; i < 100; i++) {
+        spawn_particle(&particles[i]);
+    }
+
+    while (1) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                exit(0);
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        for (int i = 0; i < 100; i++) {
+            particle_t* particle = &particles[i];
+            particle->x += particle->vx;
+            particle->y += particle->vy;
+            particle->lifetime--;
+
+            if (particle->lifetime <= 0) {
+                spawn_particle(particle);
+            }
+
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+            SDL_RenderDrawPoint(renderer, particle->x, particle->y);
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    return 0;
+}
+
---
+++ 04/10/helper.c
@@ -0,0 +1,36 @@
+#include "helper.h"
+
+SDL_Window* Window = NULL;
+SDL_Renderer* Renderer = NULL;
+TTF_Font* icon_font = NULL;
+
+void init(void)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	TTF_Init();
+
+	Window = SDL_CreateWindow("MP3 Player", 100, 100, 407, 314, SDL_WINDOW_SHOWN);
+
+	//BEGIN ICON
+	SDL_Surface* icon;
+	icon_font = TTF_OpenFont("fontawesome-webfont.ttf", 50);
+	SDL_Color font_color_icon = { 200,0,255,255 };
+	icon = TTF_RenderGlyph_Blended(icon_font, 0xf001, font_color_icon);
+	SDL_SetWindowIcon(Window, icon);
+	SDL_FreeSurface(icon);
+	TTF_CloseFont(icon_font);
+	//END ICON
+
+	Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_SOFTWARE );
+
+}
+
+void exit_(void)
+{
+
+	SDL_DestroyRenderer(Renderer);
+	SDL_DestroyWindow(Window);
+	TTF_Quit();
+	SDL_Quit();
+
+}
